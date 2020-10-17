72ae3bff1af9efbb7378b5a17e9a66cfaea031da
Jezer <jezer.vallivattam@students.iiserpune.ac.in>
2020-10-17 20:28:57 +0530

    updated height

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
   var canvas = document.getElementById("acanvas");
   document.getElementsByClassName('theme-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
   document.getElementsByClassName('theme-info-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+  // document.body.onresize=headerSizeUpdate();
 });
+//
+// function headerSizeUpdate() {
+//
+// }
