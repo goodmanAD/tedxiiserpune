fd7aae939d8e7c6b6cd59a45d89812aad128dded
Jezer <jezer.vallivattam@students.iiserpune.ac.in>
2020-10-17 20:45:37 +0530

    updated height

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
+function headerSizeUpdate() {
+  var header = document.getElementsByClassName('header')[0];
+  var menu=document.getElementsByClassName('menu')[0];
+  var rect1 = header.getBoundingClientRect();
+  var rect2 = menu.getBoundingClientRect();
+  if (rect1.bottom - rect2.bottom < 50)
+  {
+    header.style.height = rect2.bottom+50px;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", ()=>{
   var canvas = document.getElementById("acanvas");
   document.getElementsByClassName('theme-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
   document.getElementsByClassName('theme-info-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
-  // document.body.onresize=headerSizeUpdate();
+  document.body.onresize=headerSizeUpdate();
 });
-//
-// function headerSizeUpdate() {
-//
-// }
+
+window.addEventListener('orientationchange', headerSizeUpdate());
