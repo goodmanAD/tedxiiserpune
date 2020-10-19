dd03bd4b919c0dfbe9b420c9026989754977f1c1
Jezer <jezer.vallivattam@students.iiserpune.ac.in>
2020-10-19 17:36:58 +0530

    Fixed background with squares and rearranged footer

--- js/preloader.js
+++ js/preloader.js
@@ -1,45 +1,72 @@
 document.addEventListener("DOMContentLoaded", ()=>{
   var canvas = document.getElementById("acanvas");
 
-  var w = canvas.width = 500;
-  var h = canvas.height = 500;
+  var w = canvas.width = 300;
+  var h = canvas.height = 300;
 
   var context = canvas.getContext("2d");
 
-  for(i=0;i<w/8;i++) {
-      for(j=0;j<h/8;j++) {
+  for(i=0;i<w/10;i++) {
+      for(j=0;j<h/10;j++) {
 
           var t=150;
           var num = Math.floor(Math.sqrt(Math.random())*((t)+0.07));
           // num+=60;
-          var radius = Math.random()*10;
-          context.beginPath();
-          context.arc((i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
-          context.fillStyle = (num<t) ? ("rgb(" + num*0.8 + "," + num*0.9 + "," + num + ")"):("#e62b1e");
-          context.fill();
-          if ((i*123 + j)%w < radius) {
-            context.beginPath();
-            context.arc(w+(i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
-            context.fillStyle = (num<t) ? ("rgb(" + num*0.8 + "," + num*0.9 + "," + num + ")"):("#e62b1e");
-            context.fill();
+          var radius = Math.random()*40;
+          var rotation=2*Math.random()*Math.PI
+          context.fillStyle = ("rgba(" + (num+20)*0.8 + "," + (num+20)*0.9 + "," + (num+20) + ", 0.4)");
+          // context.beginPath();
+          // context.arc((i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
+          context.translate((i*123 + j)%w, (j*123)%h);
+          context.rotate(rotation);
+          context.fillRect(0, 0, radius, radius);
+          context.rotate(-rotation);
+          context.translate(-((i*123 + j)%w), -((j*123)%h));
+
+          // context.fill();
+          if ((i*123 + j)%w < radius*(2)) {
+            // context.beginPath();
+            // context.arc(w+(i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
+            // context.fillStyle = ("rgba(255,0,0, 0.4)");
+            context.translate(w+(i*123 + j)%w, (j*123)%h);
+            context.rotate(rotation);
+            context.fillRect(0, 0, radius, radius);
+            context.rotate(-rotation);
+            context.translate(-w-((i*123 + j)%w), -((j*123)%h));
+            // context.fill();
           }
-          else if (w-((i*123 + j)%w) < radius) {
-            context.beginPath();
-            context.arc(w-(i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
-            context.fillStyle = (num<t) ? ("rgb(" + num*0.8 + "," + num*0.9 + "," + num + ")"):("#e62b1e");
-            context.fill();
+          if (w-((i*123 + j)%w) < radius*(2)) {
+            // context.beginPath();
+            // context.arc(w-(i*123 + j)%w, (j*123)%h, radius, 0, 2*Math.PI);
+            // context.fillStyle = ("rgba(0,255,0, 0.4)");
+            context.translate(-w+((i*123 + j)%w), (j*123)%h);
+            context.rotate(rotation);
+            context.fillRect(0, 0, radius, radius);
+            context.rotate(-rotation);
+            context.translate(w-((i*123 + j)%w), -((j*123)%h));
+            // context.fill();
           }
-          else if ((j*123)%h < radius) {
-            context.beginPath();
-            context.arc((i*123 + j)%w, h+(j*123)%h, radius, 0, 2*Math.PI);
-            context.fillStyle = (num<t) ? ("rgb(" + num*0.8 + "," + num*0.9 + "," + num + ")"):("#e62b1e");
-            context.fill();
+          if ((j*123)%h < radius*(2)) {
+            // context.beginPath();
+            // context.arc((i*123 + j)%w, h+(j*123)%h, radius, 0, 2*Math.PI);
+            // context.fillStyle = ("rgba(0,0,255, 0.4)");
+            context.translate((i*123 + j)%w, h+(j*123)%h);
+            context.rotate(rotation);
+            context.fillRect(0, 0, radius, radius);
+            context.rotate(-rotation);
+            context.translate(-((i*123 + j)%w), -h-((j*123)%h));
+            // context.fill();
           }
-          else if (h-((j*123)%h) < radius) {
-            context.beginPath();
-            context.arc((i*123 + j)%w, h-(j*123)%h, radius, 0, 2*Math.PI);
-            context.fillStyle = (num<t) ? ("rgb(" + num*0.8 + "," + num*0.9 + "," + num + ")"):("#e62b1e");
-            context.fill();
+          if (h-((j*123)%h) < radius*(2)) {
+            // context.beginPath();
+            // context.arc((i*123 + j)%w, h-(j*123)%h, radius, 0, 2*Math.PI);
+            // context.fillStyle = ("rgba(255,0,255, 0.4)");
+            context.translate((i*123 + j)%w, -h+(j*123)%h);
+            context.rotate(rotation);
+            context.fillRect(0, 0, radius, radius);
+            context.rotate(-rotation);
+            context.translate(-((i*123 + j)%w), h-((j*123)%h));
+            // context.fill();
           }
       }
   }
