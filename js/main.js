8bea17363c9ff96d36c189e1d27c42ab23506995
jezerojojo14 <jezer.vallivattam@students.iiserpune.ac.in>
2021-08-11 20:46:44 +0530

    streaming page first commit

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ function headerSizeUpdate() {
 
 document.addEventListener("DOMContentLoaded", ()=>{
   var canvas = document.getElementById("acanvas");
-  document.getElementsByClassName('theme-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
-  document.getElementsByClassName('theme-info-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+  if (document.getElementsByClassName('theme-container')[0]) {
+    document.getElementsByClassName('theme-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+    document.getElementsByClassName('theme-info-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+  }
   document.body.onresize=headerSizeUpdate();
 });
 
